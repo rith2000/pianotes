28b9fdb0d56eecef89b026bdfc8104afb771c3db
DavidWChen <david.willisis.chen@gmail.com>
2019-02-12 13:18:21 -0800

    use notation file

--- src/ScoreDisplay.js
+++ src/ScoreDisplay.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Notation } from 'react-abc';
 
-const notation = 'd3 _B =B_A =A2 _A2 _B=B G2 c ';
-
+//const notation = 'd3 _B =B_A =A2 _A2 _B=B G2 c ';
+import notation from './notation';
 export default () => (
   <div>
     <Notation notation={notation} />
